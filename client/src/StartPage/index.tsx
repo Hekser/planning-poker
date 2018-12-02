f6e1d4cb0aa27d7dacde283e96c49dd7a8a0b754
cklaymen <cklaymen@outlook.com>
2018-12-02 12:47:23 +0100

    Styled components

--- client/src/StartPage/index.tsx
+++ client/src/StartPage/index.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { withRouter } from "react-router";
 
 import { Button } from "../Common/Button";
+import { Container } from "./styled";
 
 export const StartPage = withRouter(props => (
-  <div>
+  <Container>
     _START_PAGE_
     <Button onClick={() => {}}>Stwórz nowy pokój</Button>
-  </div>
+  </Container>
 ));
---
+++ client/src/StartPage/styled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Container = styled.div`
+  max-width: 600px;
+  max-height: 400px;
+`;
